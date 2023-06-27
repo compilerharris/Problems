7c7417c372382f34fe60f6c1cd66c9494044cfef
Haris Shaikh <compilerharris@gmail.com>
2023-06-27 18:28:17 +0530

    initial commmit

---
+++ Two Sum/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var answer = [];
+    for(let i=0;i<nums.length-1;i++){
+        for(let j=i+1; j<nums.length;j++){
+            if( nums[i]+nums[j] == target){
+                answer[0] = i;
+                answer[1] = j;
+                break;
+            }
+        }
+    }
+    return answer;
+};
